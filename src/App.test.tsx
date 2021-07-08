0cb3566de6a09037825af88306a98ec020df54fa
caaatisgood <almigod0718@gmail.com>
2021-07-08 10:02:28 +0800

    Update README

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,6 @@ import App from "./App";
 
 test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/Hello, alpaca generator!/i);
+  const linkElement = screen.getByText(/alpaca generator/i);
   expect(linkElement).toBeInTheDocument();
 });
